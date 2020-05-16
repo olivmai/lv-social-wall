0553cfba5bd544f5844d456d6718a38ffca9758c
Olivier Mairet <oliviermairet@gmail.com>
2020-05-16 17:06:39 +0200

    [WIP] add tests

---
+++ tests/unit/MercurePublisherTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class MercurePublisherTest extends TestCase
+{
+    public function testSomething()
+    {
+        $this->assertTrue(true);
+    }
+}
